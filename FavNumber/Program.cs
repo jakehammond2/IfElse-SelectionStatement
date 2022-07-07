c9f59bc658cca0bd9f637defc9aac69fee4e44e2
Jake Hammond <jakehammond226@gmail.com>
2022-07-07 11:03:24 -0500

    Fav Number

---
+++ FavNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("Guess a random number from 1 - 1000");
+var r = new Random();
+var randomNumber = r.Next(1, 1000);
+var userInput = int.Parse(Console.ReadLine());
+
+
+if (userInput > randomNumber)
+{
+    Console.WriteLine("too low");
+}
+
+else if (userInput < randomNumber)
+{
+    Console.WriteLine("too high");
+}
+
+else
+{
+    Console.WriteLine("You guessed it!!!!");
+}
